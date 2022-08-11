ffde2b3b95f0bb61aef1c0b4ff40825f60839d76
Hitendra Verma <hiten200verma@gmail.com>
2022-08-11 16:13:08 +0530

    Getter setter and Constructor added in all Entity

--- OnlineShoppingApp/src/main/java/com/masai/model/Address.java
+++ OnlineShoppingApp/src/main/java/com/masai/model/Address.java
@@ -1,5 +1,17 @@
 package com.masai.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+
 public class Address {
 
 	Integer addressId;
