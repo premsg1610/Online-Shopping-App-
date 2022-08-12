5460f65f4447b22f1bce7d687f23a70a1f59efb3
unknown <rupeshthakur8844@gmail.com>
2022-08-12 19:51:20 +0530

    added some of the exception handler inside global exception

--- OnlineShoppingApp/src/main/java/com/masai/exceptions/GlobalExceptionHandler.java
+++ OnlineShoppingApp/src/main/java/com/masai/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,78 @@
 package com.masai.exceptions;
 
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	if any logical error happens than this exception will be thrown.
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<MyErrorDetails> logicalException(Exception e, WebRequest wr){
+		
+		System.out.println("Inside the Exception Handler...");
+		
+		MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(), e.getMessage(), wr.getDescription(false));
+		
+		ResponseEntity<MyErrorDetails> re = new ResponseEntity<>(err,HttpStatus.BAD_REQUEST);
+		
+		return re;
+		
+	}
+	
+	
+	
+	
+//	if user passes wrong api than this exception will be thrown automatically by spring boot
+	
+	@ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<MyErrorDetails> wrongApiException(NoHandlerFoundException e, WebRequest wr){
+		
+		System.out.println("Inside the NoHandlerFoundException Handler...");
+		
+		MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(), e.getMessage(), wr.getDescription(false));
+		
+		ResponseEntity<MyErrorDetails> re = new ResponseEntity<>(err,HttpStatus.BAD_REQUEST);
+		
+		return re;
+		
+	}
+	
+	
+	
+//	if user don't pass right argument than this exception will be thrown....
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<MyErrorDetails> myIllegalHandler4(MethodArgumentNotValidException ie,WebRequest req) {
+		
+			System.out.println("inside MethodArgumentNotValidException Handler...");
+			
+			MyErrorDetails err=new MyErrorDetails(LocalDateTime.now(), ie.getMessage(), req.getDescription(false));
+			
+			ResponseEntity<MyErrorDetails> re=new ResponseEntity<>(err,HttpStatus.BAD_REQUEST);
+			
+			return re;
+	
+	}
+	
+	
 }
