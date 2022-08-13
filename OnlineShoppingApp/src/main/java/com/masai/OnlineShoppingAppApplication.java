f33a692b0bd7988323d629a5e488cd20319abe33
Hitendra Verma <hiten200verma@gmail.com>
2022-08-13 18:34:41 +0530

    Comment removed

--- OnlineShoppingApp/src/main/java/com/masai/OnlineShoppingAppApplication.java
+++ OnlineShoppingApp/src/main/java/com/masai/OnlineShoppingAppApplication.java
@@ -2,8 +2,6 @@ package com.masai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 @SpringBootApplication
 public class OnlineShoppingAppApplication {
@@ -12,10 +10,5 @@ public class OnlineShoppingAppApplication {
 		SpringApplication.run(OnlineShoppingAppApplication.class, args);
 	}
 
-	
-//	@Bean(name = "entityManagerFactory")
-//	public LocalSessionFactoryBean sessionFactory() {
-//	LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-//	return sessionFactory;
-//	}
+
 }
